312ac56b961cf9112722b0c06367c5ddf9a000df
nepikn <nepikn@gmail.com>
2023-11-15 07:18:27 +0800

    ConstructorType

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,5 +1,28 @@
+import Film from "./film";
+import View from "./view";
+import useLocalStorage from "./useLocalStorage";
+
 export type SoldoutFilm = {
   name: string;
   start: string;
   venue: string;
 };
+// interface FilmName {
+//   [Id: Film["id"]]: Film["name"] | null;
+// }
+
+export interface Join {
+  [Name: Film["name"]]: Film["id"] | null;
+}
+export interface ViewGroup {
+  id: string;
+  name: string;
+  title: string;
+  views: View[];
+  setViews: ReturnType<typeof useLocalStorage<View[]>>[1];
+}
+export interface FilterConfig {
+  type: "name" | "date";
+  key: string | number;
+  isCheck: boolean;
+}
